f1fb56816d6db40320326a4302a27b000ef11005
RMH <rory.hennessey@gmail.com>
2019-03-21 13:39:30 -0400

    updated start node

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 4000;
 const greeting = process.env.GREETING;
+
 server.listen(port, () => {
   console.log(`\n*** ${greeting} http://localhost:${port} ***\n`);
 });
